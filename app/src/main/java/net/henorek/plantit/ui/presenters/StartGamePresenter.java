2f70a496a49e2eeef0796476dca938aa832c06db
jarek.jankowski <jarek.jankowski>
2016-01-25 13:18:13 +0100

    Added missing class.

---
+++ app/src/main/java/net/henorek/plantit/ui/presenters/StartGamePresenter.java
@@ -0,0 +1,8 @@
+package net.henorek.plantit.ui.presenters;
+
+import net.henorek.plantit.ui.base.presenters.BasePresenter;
+import net.henorek.plantit.ui.interfaces.IStartGameView;
+import net.henorek.plantit.ui.presenters.interfaces.IStartGamePresenter;
+
+public class StartGamePresenter extends BasePresenter<IStartGameView> implements IStartGamePresenter {
+}
